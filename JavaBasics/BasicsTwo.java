7a323af77d2bf03f351576bbf35b78d49851b3f8
kolynzb <collinsbenda360@gmail.com>
2022-01-22 13:02:29 +0300

    second lesson

--- JavaBasics/BasicsTwo.java
+++ JavaBasics/BasicsTwo.java
@@ -1,5 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
 /**
- * @author kolynz
+ *
+ * @author kolynzb
  */
 import java.util.Scanner;
 
@@ -7,47 +13,47 @@ public class BasicsTwo {
 
     public static void main(String[] args) {
         // this is for printing to the console
-        System.out.println("hello world");
+//        System.out.println("hello world");
         String firstName = "Joshua";
         int age = 12;
         float weight = 68.88f;
         boolean isAdult = true;
 
-        System.out.println("My name is " + firstName + " i am " + age + "years old" );
-
+//        System.out.println("My name is " + firstName + "\n i am " + age + "years old");
         //the arithmetic operators are + - * / % 
         int arith = 5 % 2;
 
-        System.out.println(arith);
+//        System.out.println(arith);
 
         int sum1 = 100 + 50;        // 150 (100 + 50)
         int sum2 = sum1 + 250;      // 400 (150 + 250)
         int sum3 = sum2 + sum2;
-        System.out.println(sum3);
-        System.out.println(++sum3); //increment
+//        System.out.println(sum3);
+//        System.out.println(++sum3); //increment
 
         int x = 10;
         x += 5;
-        System.out.println(x);
+//        System.out.println(x);
         //the comparison operators are == != > < >= <=
 
-        System.out.println(18 < age);
-        System.out.println(!isAdult);
+//        System.out.println(18 < age);
+//        System.out.println(!isAdult);
 
         String username = "sherine";
         String password = "password";
+
 
         Scanner ui = new Scanner(System.in);
         Scanner pi = new Scanner(System.in);
 
         String userInput = ui.nextLine();
         String passInput = pi.nextLine();
-        
-        // System.out.println( password.equals(passInput));
+
+//        System.out.println( password.equals(passInput));
        
+
         if (username.equals(userInput) &&  password.equals(passInput)) {
             System.out.println("Welcome  " + username);
-
         } else if ( !username.equals(userInput)) {
             System.out.println("Wrong username");
         } else if (  !password.equals(passInput) ) {
@@ -55,6 +61,21 @@ public class BasicsTwo {
         } else {
             System.out.println("Not Authorized");
         }
+        
+        
+        int a = 3;
+        int b = 5;
+        int result = a + b;
+//        System.out.println(result);
+
+        int i = 0;
+//        while (i < 5) {
+//            System.out.println(i);
+//            i++;
+//        }
+   
+        
+      
     }
 
 }
